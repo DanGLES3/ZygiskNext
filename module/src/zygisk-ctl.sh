7fefb55a1032d1636ee97f13456f3e1118dfca31
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2024-05-15 18:01:29 +0300

    Revert "Use random /dev/ directory"
    This reverts commit 0bc2ad6b7a647b5b6950a99012fd103dc044fa59.

--- module/src/zygisk-ctl.sh
+++ module/src/zygisk-ctl.sh
@@ -1,6 +1,6 @@
 MODDIR=${0%/*}/..
 
 export TMP_PATH=/sbin
-[ -d /sbin ] || export TMP_PATH="$(cat $MODDIR/rand)"
+[ -d /sbin ] || export TMP_PATH=/debug_ramdisk
 
 exec $MODDIR/bin/zygisk-ptrace64 ctl $*
